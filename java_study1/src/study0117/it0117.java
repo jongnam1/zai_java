45c9ca6906af642118da91221c39e8c0e567ac66
jongnam1 <oklego2002@naver.com>
2025-01-17 12:49:19 +0900

    변수,데이터,형변환

---
+++ java_study1/src/study0117/it0117.java
@@ -0,0 +1,18 @@
+package study0117;
+
+public class it0117 {
+
+	public static void main(String[] args) {
+	
+    int num = 20;
+    float pi=3.14f;
+    
+    pi = num; // 자동 형변환
+    num = (int)pi; // 강제 형변환 강제로 타입을 일시적으로 바꿈, 크기를 바꾼다.
+    char ch = (char)pi;
+    
+    
+    
+	}
+
+}
