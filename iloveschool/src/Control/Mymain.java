a402a10e4ab9e5834c63c338d02c9da781c4ed17
jongnam1 <oklego2002@naver.com>
2025-03-11 12:49:48 +0900

    0311 Java 학교찾기

--- iloveschool/src/Control/Mymain.java
+++ iloveschool/src/Control/Mymain.java
@@ -18,6 +18,30 @@ public class Mymain {
     	int num = sc.nextInt();
     	return num;
     }
+    
+    public static int subMenu(SchoolService schoolService) {
+    
+    	System.out.println("\n ===== 아이러브스쿨======\n");
+    	System.out.println("1. 학교찾기");
+    	System.out.println("2. 동창회");
+    	System.out.println("3. 소모임");
+    	System.out.println("4. 종료");
+    	System.out.print("선택 :");
+    	int num = sc.nextInt();
+    	
+    	if(num == 1) {
+    		schoolService.searchSchool();
+    	}else if( num == 2) {
+    		schoolService.myReunion();
+    	}else if (num ==3) {
+    		schoolService.smallGather();
+    	}else {
+    		return 0;
+    	}
+    	return subMenu(schoolService);
+    }
+    
+    
 	public static void main(String[] args) {
 		// 서비스 클래스들을 통해 로그인과 학교 찾기를 할것이다.
 		MemberService memberService = new MemberService();
@@ -27,7 +51,11 @@ public class Mymain {
 		while(true) {
 			switch(menu()) {
 			case 1: //로그인
-				memberService.signIn();
+				if (memberService.signIn()) {// 로그인 성공
+				     subMenu(schoolService);
+				     return;
+			    }else// 로그인 실패 
+					System.out.println("아이디 또는 비밀번호가 잘못되었습니다.");
 				break;
 			case 2: //회원가입
 				memberService.signUp();
@@ -47,4 +75,67 @@ public class Mymain {
 	}
 
 }
+
+/*
+ int fac(int a) {
+ return a * fac(a-1);
+ }
+ 
+ fac(5); 1번째 메서드
+ a->5
+ return 5 * fac(5-1);
+ 
+ fac(4) 2번쟤 메서드
+  a->4
+  return 4 * fac(4-1);
+  
+  fac(3) 3번쟤 메서드 
+  a -> 3
+  return 3 * fac(3-1);4번째 메서드
+   a->2
+   return 2* fac(2-1);5번째 메서드
+    a->1
+    return 1;
+ 
+      
+      
+      float aa( int b) {
+        
+        if( b>5)
+              aa( b-1);
+         return b/2.0;   
+      }
+      
+      aa( 6 );
+      
+      
+      int i = 7;
+      for(; i>5; i--);
+         
+         
+      for(int i = 1; i<=5; i++){
+         sum = sum+i;
+      }
+      System.out.println(sum);
+      
+      int sum( int a){
+      
+            if(a==1) return 1;      
+            return a + sum(a-1);     
+      }
+      
+      
+      
+      System.out.println( sum(5) );
+     
+ 
+ 
+ */
+
+
+
+
+
+
+
 
--- iloveschool/src/DAO/SchoolDAO.java
+++ iloveschool/src/DAO/SchoolDAO.java
@@ -1,5 +1,40 @@
 package DAO;
 
-public class SchoolDAO {
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import VO.School;
+
+public class SchoolDAO extends DBconnect{
+
+	public ArrayList<School> searchSelect(String searchWord) {
+		
+		String sql = "select * from school where school_name like "+ "'%"+ searchWord+ "%'";
+		ArrayList<School> list = new ArrayList<School>();
+		
+		try {
+			st = conn.createStatement();
+			rs = st.executeQuery(sql);
+			while( rs.next( )) {
+				School data =new School(
+						
+		rs.getInt("id"),rs.getString("diff"), rs.getString("school_name"),
+		rs.getString("address"), rs.getInt("post_code"),
+		rs.getString("tel"), rs.getString("fax"),
+		rs.getString("education_office"), rs.getString("public_private"),
+		rs.getString("homepage")
+						);
+				
+				list.add(data);	
+				
+			}
+			
+		}catch(SQLException e) {
+			System.out.println(" 학교 검색 실패 ");
+		}
+		
+		
+		return list;
+	}
 
 }
