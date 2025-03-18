cda66892ccab29ba1858fa25e42a765eb4b7f2af
jongnam1 <oklego2002@naver.com>
2025-03-18 17:44:07 +0900

    0318 jsp model1

---
+++ jsp2/src/main/java/DAO/MemberDao.java
@@ -0,0 +1,50 @@
+package DAO;
+
+import java.sql.*;
+
+public class MemberDao {
+	
+	private Connection conn;
+	private PreparedStatement pt;
+	private ResultSet rs;
+	
+	public MemberDao() {
+		connect();
+	}
+	
+	
+	public boolean loginCheck(String email, String pw) {
+		String sql = "select * from test_member "+ "where email=? and password=?";
+		
+	try {
+		pt= conn.prepareStatement(sql);
+		pt.setString(1, email);
+		pt.setString(2, pw);
+		rs=pt.executeQuery();
+		if(rs.next()) return true; // 로그인 성공
+	}catch(SQLException e) {
+		System.out.println("아이디 비번 조회 실패");
+		e.printStackTrace();
+	}
+	return false; // 로그인 실패 -아이디또는 비번틀림
+	
+	}
+	
+	private void connect() {
+		try{
+			
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			String user="dvp";
+			String password="1234";
+			String url="jdbc:mysql://210.219.170.244:3306/dvp";
+			conn = DriverManager.getConnection( url, user,password);
+			
+
+		}catch( Exception e){
+			System.out.println("드라이버 로드 및 실패");
+			e.printStackTrace();
+		}
+	}
+}
--- jsp2/src/main/java/control/Main.java
+++ jsp2/src/main/java/control/Main.java
@@ -1,10 +1,13 @@
 package control;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import service.MemberService;
+
 import java.io.IOException;
 
 /**
@@ -25,16 +28,29 @@ public class Main extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+//		response.sendRedirect("/jsp2/member/signIn.jsp"); // <-이 방식으로는 데이터 전달이 안됨
+		
+
+//		response.getWriter().append("<h1> i love pizza </h1>");
+		
+		RequestDispatcher rd = request.getRequestDispatcher("member/signIn.jsp"); 
+		rd.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// 로그인 처리와 메인페이지 제공
+		MemberService ms = new MemberService();
+		ms.login(request);
+		
+		
+		
+		
+		
+		response.sendRedirect("/jsp2"); // 메인페이지 - webapp밑에 있는 index.jsp	
 	}
 
 }
